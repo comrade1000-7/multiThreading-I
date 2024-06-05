559f9d8124af1087810d0fb18e7a95134038d2f9
Vasily Parshentsev <vparshentsev@gmail.com>
2024-06-05 23:21:52 +0300

    add task one

---
+++ src/main/java/multithreading_I/AtomicIntegerCounterImp.java
@@ -0,0 +1,23 @@
+package multithreading_I;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicIntegerCounterImp implements SiteVisitCounter{
+
+    AtomicInteger counterVisitors = new AtomicInteger(0);
+
+    @Override
+    public void incrementVisitCount() {
+        try {
+            counterVisitors.getAndIncrement();
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public int getVisitCount() {
+        return counterVisitors.get();
+    }
+}
---
+++ src/main/java/multithreading_I/SynchronizedBlockCounterImpl.java
@@ -0,0 +1,25 @@
+package multithreading_I;
+
+public class SynchronizedBlockCounterImpl implements SiteVisitCounter{
+
+    Integer counterVisitors = 0;
+
+    @Override
+    public void incrementVisitCount() {
+        synchronized (this) {
+            try {
+                counterVisitors++;
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    public int getVisitCount() {
+        synchronized (this) {
+            return counterVisitors;
+        }
+    }
+}
